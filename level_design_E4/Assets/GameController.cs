1ad2d6894bd3b6c9f69429081a55673eef997daf
Ana Hernández <anaisabel14hb@gmail.com>
2020-01-19 14:24:18 +0100

    adding collectables

---
+++ level_design_E4/Assets/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private uint goal = 1;
+    public uint coins = 0;
+    public uint diamonds = 0;
+    public uint stars = 0;
+
+    public void DiamondCollected(GameObject diamond)
+    {
+        ++diamonds;
+    }
+    public void CoinCollected(GameObject coin)
+    {
+        ++coins;
+    }
+    public void StarCollected(GameObject star)
+    {
+        ++stars;
+        if(stars == goal)
+        {
+            //WIN;
+        }
+    }
+}
