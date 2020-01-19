ed33989cb0a70dff7b72ff6e33e2ca39bc3a58cd
Ana Hernández <anaisabel14hb@gmail.com>
2020-01-19 16:00:23 +0100

    quit application after win

--- level_design_E4/Assets/Scripts/GameController.cs
+++ level_design_E4/Assets/Scripts/GameController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    private uint goal = 1;
+    public uint goal = 1;
+    public float seconds_until_exit = 3.0f;
     public uint coins = 0;
     public uint diamonds = 0;
     public uint stars = 0;
@@ -22,7 +23,18 @@ public class GameController : MonoBehaviour
         ++stars;
         if(stars == goal)
         {
-            //WIN;
+            gameObject.GetComponent<UnityStandardAssets.Vehicles.Ball.BallUserControl>().enabled = false;
+            gameObject.GetComponent<UnityStandardAssets.Vehicles.Ball.BallUserControl>().enabled = false;
+            gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+            Invoke("Close", seconds_until_exit);
         }
+    }
+
+    void Close()
+    {
+        Debug.Log("WIN");
+        Application.Quit();
     }
 }
