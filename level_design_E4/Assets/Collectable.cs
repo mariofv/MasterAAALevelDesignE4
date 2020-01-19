1bb06b411b5a90d2762d372b3426dd7de18198f7
Ana Hernández <anaisabel14hb@gmail.com>
2020-01-19 13:44:56 +0100

    Add collectable prefabs and initial logic

---
+++ level_design_E4/Assets/Collectable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            switch (gameObject.tag)
+            {
+                case "Diamond":
+                    other.GetComponent<GameController>().DiamondCollected(gameObject);
+                    break;
+                case "Coin":
+                    other.GetComponent<GameController>().CoinCollected(gameObject);
+                    break;
+                case "Star":
+                    other.GetComponent<GameController>().StarCollected(gameObject);
+                    break;
+            }
+            gameObject.SetActive(false);
+        }
+       
+    }
+}
